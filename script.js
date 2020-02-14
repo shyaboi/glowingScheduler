f34c9c9b99c441b851a3b70f105154cfb373571a
Ian <ian.shane.sears@gmail.com>
2020-02-13 18:35:53 -0700

    dope fade in and note1 saving to local storage

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var notes = document.getElementById("hrChunk1").value;
+var noteOne = '';
+console.log(noteOne)
 
+$(function(){
+$("#hrChunks").fadeIn(1500);
+});
 
+$(document).ready(function(){
+  $("#saveBtn").click(function(){
+      var comment = $.trim($("#hrChunk1").val());
+     localStorage.setItem("#note", comment)
+    });
+ 
+});
+// $( ".btn" ).click(function() {
+//     $( this ).toggleClass('myButtonClicked' );
+//     alert('Changes saved M8!')
+//   });
 
+//   $(document).ready(function () {
+//     var savesnotesbtn = document.getElementById("saveBtn");
 
-$( ".btn" ).click(function() {
-    $( this ).toggleClass('myButtonClicked' );
-    alert('Changes saved M8!')
-  });
-
-  $(document).ready(function () {
-    var savesnotesbtn = document.getElementById("saveBtn");
-
-    //FILL TEXT AREAS WITH NOTES
-    for (var i = 1; i < 9; i++) {
-      $("#note" + i + "input").val(localStorage.getItem("note" + i));
-    }
-
+//     //FILL TEXT AREAS WITH NOTES
+//     for (var i = 1; i < 9; i++) {
+//       $("#note" + i + "input").val(localStorage.getItem("note" + i));
+//     }
     function saveNotes() {
       // Save data to localstorage
       for (var i = 1; i < 9; i++) {
-        localStorage.setItem("note" + i, $("#note" + i + notes).val());
-      }
-      $(savesnotesbtn).click(function(){
-          saveNotes()
-      })
-    };
+        localStorage.setItem("note" + i, $("#note" + i + noteOne).val());
+      }};
 window.onload = function(){
     saveNotes()
 };
-});
